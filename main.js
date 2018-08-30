f8ccc7ac7e302dc67e5ba47f7d83166c6d8d7953
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-30 08:39:54 +0300

    Create getQuote function

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ var tweetQuote = document.getElementById("tweet-quote");
 
 newQuote.addEventListener('click', getQuote)
 
+function getQuote() {
+    axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then(res => {
+        console.log(res.data.quotes[10]);
+    })
+}
