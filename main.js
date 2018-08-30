9845297c647f3266be7eefeb85aecca0cf5c1c9f
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-30 08:37:38 +0300

    Import Axios and main.js and cache DOM elements

---
+++ main.js
@@ -0,0 +1,5 @@
+var text = document.getElementById("text");
+var author = document.getElementById("author");
+var newQuote = document.getElementById("new-quote");
+var tweetQuote = document.getElementById("tweet-quote");
+
