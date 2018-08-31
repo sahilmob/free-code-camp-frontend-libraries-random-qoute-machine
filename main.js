a130e269b6ae3c395d6d9e88b37ee1f7548e07e9
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-31 06:09:02 +0300

    Add logic to populate text and author fields with a new quote when 'New quote' button is clicked

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ var tweetQuote = document.getElementById("tweet-quote");
 newQuote.addEventListener('click', getQuote)
 
 function getQuote() {
-    axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then(res => {
-        console.log(res.data.quotes[10]);
-    })
+    axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+        .then(({ data: { quotes } }) => {
+            const quote = quotes[Math.floor(Math.random() * (quotes.length - 0 + 1)) + 0]
+            console.log(quote);
+            text.innerText = quote.quote
+            author.innerText = quote.author
+        })
 }
