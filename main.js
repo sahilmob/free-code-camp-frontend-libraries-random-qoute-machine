667c849bc795fbe174669ade8d189f0ccde897fa
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-31 06:35:20 +0300

    Add Free code camp test script run test

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function getQuote() {
     axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
         .then(({ data: { quotes } }) => {
             const quote = quotes[Math.floor(Math.random() * (quotes.length - 0 + 1)) + 0]
-            console.log(quote);
             text.innerText = "“" + quote.quote
             author.innerText = "- " + quote.author
         })
